3b1b054c47d0220397bdd179431b1d427ce5c01a
danipoav <dapoav2002@gmail.com>
2025-06-16 14:56:47 +0200

    Adding some buttons

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout() {
             <nav className='w-full h-10 bg-gray-400'>
 
             </nav>
-            <main className=' flex-1 p-6 bg-gray-50'>
+            <main className=' flex-1 py-10 px-30 bg-gray-50'>
                 <Outlet />
             </main>
         </div>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
 
 export default function Index() {
   return (
-    <div>Index</div>
+    <div className="flex w-full px-20">
+      <div className="flex items-center justify-between w-full">
+        <input type="text" placeholder="🔍 Buscar oficina virtual..." className="w-lg h-10 border border-black rounded-lg pl-5 py-5" />
+        <button className=" cursor-pointer bg-gray-600 font-semibold text-white border rounded-xl py-3 px-10 hover:bg-gray-500">Pendientes de pago</button>
+        <button className="cursor-pointer bg-red-800 font-semibold text-white border rounded-xl py-3 px-10 hover:bg-red-700">Inactivos</button>
+        <button className="cursor-pointer bg-blue-800 font-semibold text-white border rounded-xl py-3 px-10 hover:bg-blue-700">Añadir</button>
+      </div>
+    </div>
   )
 }
